67309726804b8e8fa43be6c3d099ac0b0d8ba296
wesleybruno7 <wesleybruno7@terra.com.br>
2020-11-02 11:25:59 -0300

    adicionando desafios: react.js e react native

---
+++ Desafio 03 - ConceitosReactJS/src/App.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from "react";
+import api from './services/api';
+
+import "./styles.css";
+
+function App() {
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
+  async function handleAddRepository() {
+    
+    const response = await api.post('repositories', {
+      title: 'Desafio_03_Conceitos_do_ReactJS',
+      url: 'https://github.com/wesleybruno7/Desafio_03_Conceitos_do_ReactJS',
+      tech: ['Node.js', 'ReactJS']
+    });
+
+    setRepositories([ ...repositories, response.data ]);
+  }
+
+  async function handleRemoveRepository(id) {
+    await api.delete(`repositories/${id}`);
+
+    setRepositories(repositories.filter(
+      repository => repository.id != id
+    ));
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+        {repositories.map(repository => { 
+          return(
+            <li key={repository.id}>
+              {repository.title}
+
+              <button onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
+              </button>
+            </li>
+          )
+        })}
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
