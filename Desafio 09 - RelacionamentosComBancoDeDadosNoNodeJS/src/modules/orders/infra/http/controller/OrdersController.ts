c019741e8236736ecbc14c7ef7c689d5ad5b1adf
wesleybruno7 <wesleybruno7@terra.com.br>
2020-12-08 12:29:25 -0300

    adicionando desafio 9

---
+++ Desafio 09 - RelacionamentosComBancoDeDadosNoNodeJS/src/modules/orders/infra/http/controller/OrdersController.ts
@@ -0,0 +1,33 @@
+import { json, Request, Response } from 'express';
+
+import { container } from 'tsyringe';
+
+import CreateOrderService from '@modules/orders/services/CreateOrderService';
+import FindOrderService from '@modules/orders/services/FindOrderService';
+
+export default class OrdersController {
+  public async show(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const findOrder = container.resolve(FindOrderService);
+
+    const order = await findOrder.execute({
+      id
+    });
+
+    return response.json(order);
+  }
+
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { customer_id, products } = request.body;
+
+    const createOder = container.resolve(CreateOrderService);
+
+    const customer = await createOder.execute({
+      customer_id,
+      products,
+    });
+
+    return response.json(customer);
+  }
+}
